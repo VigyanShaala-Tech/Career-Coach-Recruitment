ee0291dc3a2209bccf0e59d9da8324948683cd9e
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-10-07 17:39:51 +0530

    Update Career_Coach_Recruitment.py

--- Scripts/Career_Coach_Recruitment.py
+++ Scripts/Career_Coach_Recruitment.py
@@ -6,13 +6,16 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import requests
-#from supabase_py import create_client,Client
 from io import StringIO  # Import StringIO directly from the io module
 from io import BytesIO
 from datetime import datetime
-#import gotrue
-from supabase import create_client, Client
+#from supabase import create_client, Client
 from supabase.client import ClientOptions
+import gspread
+from google.oauth2 import service_account
+import json
+import numpy as np
+
 
 
 # Function to get the current timestamp
@@ -43,7 +46,7 @@ Profile=st.text_input("Enter your LinkedIn profile link here")
 Institute=st.text_input("Enter your current Institute/University/Organization*")
 Current_job=st.text_input("Current Job title/Designation")
 Degree = st.selectbox('Highest degree obtained*',("B.Sc.","M.Sc.","B.E./B.Tech.","M.Tech.","B.Pharm.","M.Pharm.","MBA","Ph.D."))
-primary_key = f"{Number}_{Name}"
+#primary_key = f"{Number}_{Name}"
 
 
 
@@ -74,7 +77,9 @@ selected_options = st.multiselect("What communication languages are you comforta
 comments=['Soft skills and Professional Development Trainer |\n Teach job/career readiness Master Classes |\n Virtual engagement | 1.5 hours, 3-4 times a year','Career coach | Conduct job/career readiness workshops |\n In-person engagement |\n 2 - 4 hours, 1-2 times a year']
 comments_a=st.selectbox("How would you like to join VigyanShaala's #SheforSTEM movement?*",comments)
 
-travel_cost=st.radio('If we cover travel costs, are you willing to travel out-of-town for conducting these in-person workshops?*',('Yes','No'))
+if comments_a == "Career coach | Conduct job/career readiness workshops |\n In-person engagement |\n 2 - 4 hours, 1-2 times a year":
+     travel_cost=st.radio('If we cover travel costs, are you willing to travel out-of-town for conducting these in-person workshops?*',('Yes','No'))
+
 
 session_to_attend=['Crafting a winning STEM resume','Writing effective cover letters for STEM positions','Leveraging LinkedIn for STEM career advancement','Strategies for securing STEM internships','Mastering technical interviews','Navigating the STEM job market','Negotiating salary and benefits in STEM roles',
                    'Excelling in STEM job interviews','Developing leadership skills in STEM','Building your personal brand in STEM','Enhancing technology proficiency for STEM careers',
@@ -83,81 +88,164 @@ session_to_attend=['Crafting a winning STEM resume','Writing effective cover let
                     'Effective communication strategies for STEM fields','Polishing presentation skills for STEM presentations','Fostering teamwork in STEM settings','Critical thinking and decision-making skills for STEM professionals','Adapting to changes in the STEM industry','Project management techniques for STEM projects',
                     'Networking strategies for advancing in STEM careers','Personal and professional growth in STEM fields']
 
-session=st.multiselect('Which of the following workshops would you like to conduct? Please select all that apply.*',session_to_attend)
+session=st.multiselect('Which of the following worskshops would you like to conduct? Please select all that apply.*',session_to_attend)
 
 
 Binary=st.radio('Based on the choices you made in the previous question, do you have experience in conducting those specific workshops or similar sessions?*',('Yes','No'))
 workshop=st.text_input('Are there any other workshop topics that you would like to cover?')
-conducted=st.radio('How many such workshops have you conducted so far?*',("None","1-10","11-20",">20"))
+conducted=st.radio('How many such workshops have you conducted so far?*',("None","1-10","11-20","More than 20"))
 upload1=st.file_uploader(' If possible, please upload a sample of your work.',accept_multiple_files=False, type=["pdf", "txt"])
 call=st.radio('Would you be open to schedule a 10-15 minute call with us to discuss the structure/content of your class and tailor the workshop to our target audience?*',('Yes','No'))
 upload2=st.file_uploader('Upload your Curriculum Vitae/Resume*',accept_multiple_files=False, type=["pdf", "txt"])
-upload3=st.file_uploader('Please upload your bio and a professional headshot.',accept_multiple_files=False, type=["pdf", "txt"])
+upload3=st.file_uploader('Please upload your bio and a professional headshot.',accept_multiple_files=False, type=["pdf", "txt","jpg", "png"])
 
-if not Name or not Email_id or not Number or not Institute or not Degree or not Country or not Current_city or not selected_options or not comments_a or not travel_cost or not session or not Binary or not conducted or not call or not upload2:
+
+def create_feedback_dataframe(Name, Email_id, Number, Profile, Institute, Current_job, Degree, Country, Current_city, selected_options, comments_a, travel_cost, session, Binary, workshop, conducted, upload1, call, upload2, upload3):
+    if comments_a == "Career coach | Conduct job/career readiness workshops |\n In-person engagement |\n 2 - 4 hours, 1-2 times a year":
+        data = {
+            'Enter your full name *': [Name],
+            'Enter your email address *': [Email_id],
+            "Enter your WhatsApp number (with country code, DONOT ADD '+') *": [Number],
+            'Enter your LinkedIn profile link here': [Profile],
+            'Enter your current Institute/University/Organization *': [Institute],
+            'Current Job title/Designation': [Current_job],
+            'Highest degree obtained *': [Degree],
+            'Country you currently reside in *': [Country],
+            'Your current city *': [Current_city],
+            'What communication languages are you comfortable in?  *': [', '.join(selected_options)],
+            "How would you like to join VigyanShaala's #SheforSTEM movement?": [comments_a],
+            'If we cover travel costs, are you willing to travel out-of-town': [travel_cost],
+            'Which of the following workshops would you like to conduct? Ple': [', '.join(session)],
+            'Based on the choices you made in the previous question, do you': [Binary],
+            'Are there any other workshop topics that you would like to cove': [workshop],
+            'How many such workshops have you conducted so far? *': [conducted],
+            'If possible, please upload a sample of your work.': [upload1.name if upload1 else None],
+            'Would you be open to schedule a 10-15 minute call with us to di': [call],
+            'Upload your Curriculum Vitae/Resume *': [upload2.name if upload2 else None],
+            'Please upload your bio and a professional headshot.': [upload3.name if upload3 else None]
+        }
+    else:
+        data = {
+            'Enter your full name *': [Name],
+            'Enter your email address *': [Email_id],
+            "Enter your WhatsApp number (with country code, DONOT ADD '+') *": [Number],
+            'Enter your LinkedIn profile link here': [Profile],
+            'Enter your current Institute/University/Organization *': [Institute],
+            'Current Job title/Designation': [Current_job],
+            'Highest degree obtained *': [Degree],
+            'Country you currently reside in *': [Country],
+            'Your current city *': [Current_city],
+            'What communication languages are you comfortable in?  *':[', '.join(selected_options)],
+            "How would you like to join VigyanShaala's #SheforSTEM movement?": [comments_a],
+            'If we cover travel costs, are you willing to travel out-of-town': [''],
+            'Which of the following workshops would you like to conduct? Ple': [', '.join(session)],
+            'Based on the choices you made in the previous question, do you': [Binary],
+            'Are there any other workshop topics that you would like to cove': [workshop],
+            'How many such workshops have you conducted so far? *': [conducted],
+            'If possible, please upload a sample of your work.': [upload1.name if upload1 else None],
+            'Would you be open to schedule a 10-15 minute call with us to di': [call],
+            'Upload your Curriculum Vitae/Resume *': [upload2.name if upload2 else None],
+            'Please upload your bio and a professional headshot.': [upload3.name if upload3 else None]
+        }
+
+    df = pd.DataFrame(data)
+    return df
+
+
+if not Name or not Email_id or not Number or not Institute or not Degree or not Country or not Current_city or not selected_options or not comments_a or not session or not Binary or not conducted or not call or not upload2:
     st.error("Please fill in all the compulsory fields marked with * before proceeding.")
     st.stop()
 
 
-#Create Data Frame for the inputs in the GUI's element
+if comments_a == "Career coach | Conduct job/career readiness workshops |\n In-person engagement |\n 2 - 4 hours, 1-2 times a year":
+    combined_df = create_feedback_dataframe( Name, Email_id, Number, Profile, Institute, Current_job, Degree, Country, Current_city, selected_options, comments_a,travel_cost,session,Binary,workshop,conducted,upload1,call,upload2,upload3)
+else:
+    combined_df = create_feedback_dataframe( Name, Email_id, Number, Profile, Institute, Current_job, Degree, Country, Current_city, selected_options, comments_a,' ',session,Binary,workshop,conducted,upload1,call,upload2,upload3)
 
-def create_feedback_dataframe(primary_key, Name, Email_id, Number, Profile, Institute, Current_job, Degree, Country, Current_city, selected_options, comments_a,travel_cost,session,Binary,workshop,conducted,upload1,call,upload2,upload3):
-    data = {
-        'ID': primary_key,
-        'Enter your full name *': Name,
-        'Enter your email address *':Email_id ,
-        "Enter your WhatsApp number (with country code, DONOT ADD '+') *":Number,
-        'Enter your LinkedIn profile link here': Profile,
-        'Enter your current Institute/University/Organization *':Institute,
-        'Current Job title/Designation': Current_job ,
-        'Highest degree obtained *':Degree ,
-        'Country you currently reside in *': Country,
-        'Your current city *':Current_city ,
-        'What communication languages are you comfortable in?  *': selected_options,
-        "How would you like to join VigyanShaala's #SheforSTEM movement?": comments_a,
-        'If we cover travel costs, are you willing to travel out-of-town': travel_cost,
-        'Which of the following workshops would you like to conduct? Ple': session,
-        'Based on the choices you made in the previous question, do you ': Binary,
-        'Are there any other workshop topics that you would like to cove': workshop,
-        'How many such workshops have you conducted so far? *':conducted, 
-        'If possible, please upload a sample of your work.':upload1.name if upload1 else None ,
-        'Would you be open to schedule a 10-15 minute call with us to di':call,
-        'Upload your Curriculum Vitae/Resume *':upload2.name if upload2 else None,
-        'Please upload your bio and a professional headshot.':upload3.name if upload3 else None
-    }
 
-    feedback_df = pd.DataFrame([data])
-    return feedback_df
+supabase_credentials_url = 'https://twetkfnfqdtsozephdse.supabase.co/storage/v1/object/sign/stemcheck/career-coach-recruitment-bbd9c36f47fe.json?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdGVtY2hlY2svY2FyZWVyLWNvYWNoLXJlY3J1aXRtZW50LWJiZDljMzZmNDdmZS5qc29uIiwiaWF0IjoxNzI4MDM5OTAzLCJleHAiOjE3NTk1NzU5MDN9.kJOrZP03igSz_fTxLWjbN5svQCYcJZN3xZ9peyHAGY8&t=2024-10-04T11%3A04%3A48.676Z'
+
+# Fetch service account credentials from Supabase storage
+response = requests.get(supabase_credentials_url)
+
+if response.status_code == 200:
+# Decode the content of the response as a JSON keyfile and create service account credentials
+    service_account_info = response.json()
+    
+# Use the service account info to create credentials
+    creds = service_account.Credentials.from_service_account_info(service_account_info, scopes= ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive'])
+    client = gspread.authorize(creds)
+# Obtain an access token for the specified scope
+    access_token = creds.token
+
+        # The access_token variable now contains the access token that can be used to authenticate requests to the Google API
+else:
+    print("Failed to fetch the service account credentials. Status code:", response.status_code)
 
 
 
-#Supabase Credential to store data to database 
-
-url: str = 'https://twetkfnfqdtsozephdse.supabase.co'
-key: str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3ZXRrZm5mcWR0c296ZXBoZHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5Njk0MzcsImV4cCI6MjAzNzU0NTQzN30.D76H5RoTel0M7Wj6PTRSAXxxYGic7K25BSaeQDZqIN0'
-# Create a Supabase client
-supabase: Client = create_client(url, key, options=ClientOptions(
-    postgrest_client_timeout=10,
-    storage_client_timeout=10,
-    schema="public",
-  ))
-
-
-combined_button_text = "Submit"   
-
-#Button to push data to supabase 
-
+# Create a button in Streamlit
+combined_button_text = "Print"
 if st.button(combined_button_text):
-    feedback_df = create_feedback_dataframe(primary_key, Name, Email_id, Number, Profile, Institute, Current_job, Degree, Country, Current_city, selected_options, comments_a,travel_cost,session,Binary,workshop,conducted,upload1,call,upload2,upload3)
+    # Insert the feedback dataframe into the Google Sheet
+    sheet_key = '1LqOFw7Ho2_Y2Snb0sxgGTyRDz6IWGs58iIRbNOQfSLA'
+    #sheet = client.open_by_key(sheet_key).get_worksheet(0)  # Update with the correct sheet name or index
+    sheet = client.open('Career_Coach_Recruitment ').sheet1
+    # Get existing data and determine the next row
+    existing_data = sheet.get_all_values()
+    next_row_index = len(existing_data) + 1
+        
+    # Append the new data below the already stored data
+    data_to_insert = combined_df.values.tolist()
+    sheet.update(f'A{next_row_index}', data_to_insert)
+    st.write("Feedback data inserted successfully")
 
-    # Prepare the JSON data
-    json_data = feedback_df[['ID','Enter your full name *','Enter your email address *',"Enter your WhatsApp number (with country code, DONOT ADD '+') *",'Enter your LinkedIn profile link here','Enter your current Institute/University/Organization *','Current Job title/Designation','Highest degree obtained *','Country you currently reside in *','Your current city *','What communication languages are you comfortable in?  *',"How would you like to join VigyanShaala's #SheforSTEM movement?",'If we cover travel costs, are you willing to travel out-of-town','Which of the following workshops would you like to conduct? Ple','Based on the choices you made in the previous question, do you ','Are there any other workshop topics that you would like to cove','How many such workshops have you conducted so far? *','If possible, please upload a sample of your work.','Would you be open to schedule a 10-15 minute call with us to di','Upload your Curriculum Vitae/Resume *','Please upload your bio and a professional headshot.']].to_dict(orient='records')[0]
+    uploaded_file = upload1.getvalue() if upload1 is not None else None
 
-    table_name = "Career_Coach_Recruitment"
+    if comments_a == "Career coach | Conduct job/career readiness workshops |\n In-person engagement |\n 2 - 4 hours, 1-2 times a year":
+        json_serializable_data = create_feedback_dataframe(Name, Email_id, Number, Profile, Institute, Current_job, Degree, Country, Current_city, selected_options, comments_a, travel_cost, session, Binary, workshop, conducted, upload1, call, upload2, upload3)
+    else:
+        json_serializable_data = create_feedback_dataframe(Name, Email_id, Number, Profile, Institute, Current_job, Degree, Country, Current_city, selected_options, comments_a, '', session, Binary, workshop, conducted, upload1, call, upload2, upload3)
 
-    # Insert the JSON data into Supabase
-    response_json = supabase.table(table_name).insert([json_data]).execute()
 
+
+    def convert_numpy_arrays_to_lists(data):
+        if isinstance(data, dict):
+            return {key: convert_numpy_arrays_to_lists(value) for key, value in data.items()}
+        elif isinstance(data, np.ndarray):
+            return data.tolist()
+        else:
+            return data
+
+    # Convert NumPy arrays to lists within json_serializable_data
+    converted_data = convert_numpy_arrays_to_lists(json_serializable_data)
+
+    # Check the data type of each value in the converted_data dictionary
+    for key, value in converted_data.items():
+        print(f"Key: {key}, Value Type: {type(value)}")
+
+# Verify the content of the converted_data dictionary to ensure it contains lists
+    print(converted_data)
+
+    # Extract values as list from the dictionary
+    values_list = list(converted_data.values())
+
+# Now try to extract the values as a list
+    try:
+        values_list = list(converted_data.values())
+        print("Successfully extracted values as a list:", values_list)
+    except Exception as e:
+        print("An error occurred while trying to extract values as a list:", e)
+
+    
+    # Extract values as list from the dictionary
+    #values_list = list(converted_data.values())
+
+    # Create a dictionary from the values list
+    dict_values = {index: value for index, value in enumerate(values_list)}
+
+    create_feedback_dataframe(dict_values)
+    st.success('Data submitted to Google Sheet successfully!')
 
     #Demo code to add the google drive API . Please give it a AWS backend connection. 
     #####################
@@ -181,7 +269,7 @@ if st.button(combined_button_text):
             #creds = service_account.Credentials.from_service_account_file(
                 #json_file_path,
                 #scopes=SCOPES
-            #)
+            #) 
 
             # Build the Drive service
             #service = build('drive', 'v3', credentials=creds)
@@ -217,10 +305,4 @@ if st.button(combined_button_text):
 # Assuming uploaded_file is a file uploaded using Streamlit file uploader
 # Call the upload function
     #upload_csv(uploaded_file)
-    
-
-    
-   
-
-
     
